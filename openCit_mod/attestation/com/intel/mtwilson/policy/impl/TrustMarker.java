61e4e4d6cff4a530e9afd80e371376eb4ad1a641
alfonso <alfonso@alfonso>
2017-03-30 19:26:40 +0200

    jlh

---
+++ openCit_mod/attestation/com/intel/mtwilson/policy/impl/TrustMarker.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2013 Intel Corporation
+ * All rights reserved.
+ */
+package com.intel.mtwilson.policy.impl;
+
+/**
+ * A list of well-known markers to use when generating trust policies
+ * @author jbuhacoff
+ */
+public enum TrustMarker {
+    BIOS,
+    VMM,
+    ASSET_TAG,
+    VM,
+    LOCATION;            
+}
